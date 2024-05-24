f97a49320c2e8bfc555513399dab98493d2a892d
jeandrad1 <jesusandradeperez@gmail.com>
2024-05-24 19:37:50 +0200

    Small fix in the algo. Everything still works

--- lis_algo.c
+++ lis_algo.c
@@ -19,6 +19,7 @@ int	*ft_lis_util_helper(int max)
 	val = (int *) malloc (sizeof(int) * (max + 1));
 	if (!val)
 		ft_display_error();
+	ft_memset(val, 0, sizeof(int) * max + 1);
 	val[max - 1] = 2147483647;
 	return (val);
 }
